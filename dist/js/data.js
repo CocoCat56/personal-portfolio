9261abfdb0632600a9622060a6ab42647ac7cd65
CocoCat56 <zo7758002@gmail.com>
2021-10-01 11:32:47 +0800

    更新 index, data.js
    更新作品順序

--- dist/js/data.js
+++ dist/js/data.js
@@ -2,8 +2,8 @@ var data = [
     {
         workName: '個人作品集',
         image: './images/personalPortfolio.jpg',
-        desc: ['作品集與相關資訊', '手刻 CSS 製作響應式網頁', 'JavaScript + CSS 呈現 scroll animate'],
-        skill: 'HTML | pug | CSS | SASS | RWD | JavaScript',
+        desc: ['作品集與相關資訊', '手刻 CSS 製作響應式網頁'],
+        skill: 'HTML | pug | CSS | SASS | RWD | JavaScript | Vue.js',
         github: 'https://github.com/CocoCat56/personal-portfolio',
         web: 'https://coco-personal-portfolio.netlify.app/'
     },
@@ -16,20 +16,20 @@ var data = [
         web: 'https://coco-works-taiwan-festivals.netlify.app/#/'
     },
     {
-        workName: '雲華',
-        image: './images/teashop.jpg',
-        desc: ['以虛擬的茶葉店為主題的網站', '手刻 CSS 和 JavaScript 製作響應式網頁', 'Vue3.0 呈現茶知識、商品資訊和說明'],
-        skill: 'HTML | pug | CSS | SASS | RWD | JavaScript | Vue.js',
-        github: 'https://github.com/CocoCat56/teashop',
-        web: 'https://coco-works-teashop.netlify.app/'
-    },
-    {
         workName: 'MeowIn café',
         image: './images/meowin.jpg',
-        desc: ['以虛擬的貓咪咖啡廳為主題網站', '使用 Bootstrap 製作響應式網頁', 'jQuery + CSS 呈現 scroll animate'],
+        desc: ['網站以虛擬的貓咪咖啡廳為主題', '使用 Bootstrap 製作響應式網頁'],
         skill: 'HTML | CSS | SASS | Bootstrap | RWD | jQuery',
         github: 'https://github.com/CocoCat56/MeowIn',
         web: 'https://coco-works-meowin.netlify.app/'
+    },
+    {
+        workName: '雲華',
+        image: './images/teashop.jpg',
+        desc: ['網站以虛擬的茶葉店為主題', '手刻 CSS 和 JavaScript 製作響應式網頁'],
+        skill: 'HTML | pug | CSS | SASS | RWD | JavaScript | Vue.js',
+        github: 'https://github.com/CocoCat56/teashop',
+        web: 'https://coco-works-teashop.netlify.app/'
     },
     {
         workName: 'Plant Diary',
